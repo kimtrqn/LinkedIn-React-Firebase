02d7a3f282a229096a3768545911745409c1262d
Julie Tran <kimtran767@gmail.com>
2021-07-13 22:03:23 -0700

    added image google button

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import loginLogo from '../images/login-logo.svg';
 import loginHero from '../images/login-hero.svg';
+import googleLogo from '../images/google.svg';
 
 const Login = (props) => {
 
@@ -21,6 +22,13 @@ const Login = (props) => {
                     <h1>Welcome to your professional community</h1>
                     <img src={loginHero} alt="" />
                 </Hero>
+
+                <Form>
+                    <Google>
+                        <img src={googleLogo} alt="" />
+                        Sign in with Google
+                    </Google>
+                </Form>
             </Section>
         </Container>
     )
@@ -117,6 +125,7 @@ const Section = styled.section`
 
 const Hero = styled.div`
     width: 100%;
+    display: flex;
 
     h1 {
         padding-bottom: 0;
@@ -135,7 +144,55 @@ const Hero = styled.div`
     }
 
     img {
+        /* z-index: -1; */
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
 
     }
 `;
+
+const Form = styled.div`
+    margin-top: 100px;
+    width: 408px;
+
+    @media (max-width: 768px) {
+        margin-top: 20px;
+    }
+
+`;
+
+const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+    align-items: center;
+    height: 56px;
+    width: 100%;
+    border-radius: 28px;
+    box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), 
+                inset 0 0 0 2px (rgb(0 0 0 / 0%), 
+                inset 0 0 0 1px rgb(0 0 0 / 0%));
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 167ms;
+    font-size: 20px;
+    color: rgba(0 , 0, 0, 0.6);
+
+    &:hover {
+        background-color: rgba(207, 207, 207, 0.25);
+        color: rgba(0, 0, 0, 0.75)
+    }
+`;
+
+
 
