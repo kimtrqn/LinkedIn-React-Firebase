3d9d25e55e4ec183ebb3a6b93b09d49f12282ce1
Julie Tran <kimtran767@gmail.com>
2021-07-21 14:05:09 -0700

    create a action folder  and reducers and  added userreducer to maintain the states

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+
+const INTITIAL_STATE = {
+    user: null,
+};
+
+const userReducer = (state = INTITIAL_STATE, action) => {
+    
+    switch(action.type) {
+        default: 
+        return state;
+    }
+};
+
+export default userReducer;
